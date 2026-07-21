93607a6089909c8d0548c2ec3d749dc5104e82fa
samanthyjayanthiran <samjeya07@gmail.com>
2026-07-21 14:50:23 +0530

    develop the js code

--- about.js
+++ about.js
@@ -1,5 +1,6 @@
 const counters = document.querySelectorAll(".counter");
 
+<<<<<<< Updated upstream
 counters.forEach(counter => {
 
     let count = 0;
@@ -23,4 +24,54 @@ counters.forEach(counter => {
     };
 
     updateCounter();
+=======
+const options = {
+    threshold: 0.5
+};
+
+const observer = new IntersectionObserver(function(entries, observer){
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+
+            const counter = entry.target;
+
+            const target = +counter.dataset.target;
+
+            let count = 0;
+
+            const update = () => {
+
+                const increment = Math.ceil(target / 100);
+
+                if(count < target){
+
+                    count += increment;
+
+                    if(count > target){
+                        count = target;
+                    }
+
+                    counter.innerText = count;
+
+                    setTimeout(update,20);
+
+                }
+
+            };
+
+            update();
+
+            observer.unobserve(counter);
+
+        }
+
+    });
+
+}, options);
+
+counters.forEach(counter=>{
+    observer.observe(counter);
+>>>>>>> Stashed changes
 });
